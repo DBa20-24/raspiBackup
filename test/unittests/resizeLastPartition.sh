cd6baf08bd6dfe61411b7d465fd259fb5cab0761
framps <framp@linux-tips-and-tricks.de>
2024-04-29 21:47:57 +0200

    Implemented #750

--- test/unittests/resizeLastPartition.sh
+++ test/unittests/resizeLastPartition.sh
@@ -1,15 +1,53 @@
 #!/bin/bash
 
-TEST_FILE1="32GB.sfdisk 31268536320"
-TEST_FILE2="128GB.sfdisk 128035676160"
-TEST_FILE3="32GB_nosecsize.sfdisk 31268536320"
-TEST_FILE4="128GB_nosecsize.sfdisk 128035676160"
+#######################################################################################################################
+#
+# 	 Unit test for sfdisk resize of last partition for raspiBackup
+#
+#######################################################################################################################
+#
+#    Copyright (c) 2024 framp at linux-tips-and-tricks dot de
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#######################################################################################################################
 
-TEST_FILES=("$TEST_FILE1" "$TEST_FILE2" "$TEST_FILE3" "$TEST_FILE4")
+source ../../raspiBackup.sh
+
+testFile=$(mktemp)
+
+function test_createResizedSFDisk() {
+
+	local partitionSizes=($(createResizedSFDisk $1 $2 $3 $4 $5))
+	local old=${partitionSizes[0]}
+	local new=${partitionSizes[1]}
+
+	resizedSize="$(calcSumSizeFromSFDISK "$3")"
+
+	if (( resizedSize != $2 || $4 != $old || $5 != $new )); then
+		echo -n "FAILED --- "
+	else
+		echo -n "SUCCESS --- "
+	fi
+
+	echo "$1: $resizedSize ($(bytesToHuman $resizedSize)) --- $(bytesToHuman $old):$(bytesToHuman $new)"
+
+}
 
 function test_calcSumSizeFromSFDISK() {
 
-	local size="$(calcSumSizeFromSFDISK $1)"
+	local size="$(calcSumSizeFromSFDISK "$1")"
 
 	if (( size != $2 )); then
 		echo -n "FAILED --- "
@@ -21,71 +59,17 @@ function test_calcSumSizeFromSFDISK() {
 
 }
 
-function bytesToHuman() {
-	local b d s S
-	local sign=1
-	b=${1:-0}; d=''; s=0; S=(Bytes {K,M,G,T,E,P,Y,Z}iB)
-	if (( b < 0 )); then
-		sign=-1
-		(( b=-b ))
-	fi
-	while ((b > 1024)); do
-		d="$(printf ".%02d" $((b % 1024 * 100 / 1024)))"
-		b=$((b / 1024))
-		let s++
-	done
-	if (( sign < 0 )); then
-		(( b=-b ))
-	fi
-	echo "$b$d ${S[$s]}"
-}
+echo "--- test_calcSumSizeFromSFDISK ---"
+test_calcSumSizeFromSFDISK 32GB.sfdisk 31268536320
+test_calcSumSizeFromSFDISK 32GB_nosecsize.sfdisk 31268536320
+test_calcSumSizeFromSFDISK 128GB.sfdisk 128035676160
+test_calcSumSizeFromSFDISK 128GB_nosecsize.sfdisk 128035676160
+echo
+echo "--- test_createResizedSFDisk ---"
+test_createResizedSFDisk "128GB.sfdisk" 31268536320 $testFile 126953545728 30186405888
+test_createResizedSFDisk "128GB_nosecsize.sfdisk" 31268536320 $testFile 126953545728 30186405888
+test_createResizedSFDisk "32GB.sfdisk" 128035676160 $testFile 30186405888 126953545728
+test_createResizedSFDisk "32GB_nosecsize.sfdisk" 128035676160 $testFile 30186405888 126953545728
 
-function calcSumSizeFromSFDISK() { # sfdisk file name
+rm $testFile
 
-	local file="$1"
-
-# /dev/mmcblk0p1 : start=     8192, size=    83968, Id= c
-# or
-# /dev/sdb1 : start=          63, size=  1953520002, type=83
-
-	local partitionregex="/dev/.*[p]?([0-9]+)[^=]+=[^0-9]*([0-9]+)[^=]+=[^0-9]*([0-9]+)[^=]+=[^0-9a-z]*([0-9a-z]+)"
-	local sumSize=0
-	local sectorSize
-
-	sectorSize=$(grep "^sector-size:" $file)
-	if (( $? )); then
-		sectorSize=512	# not set in buster and earlier, use default
-	else
-		sectorSize=$(cut -f 2 -d ' ' <<< "$sectorSize")
-		if [[ -z $sectorSize ]]; then
-			echo "assertion 1 failed"
-			exit
-		fi
-	fi
-
-	local line
-	line="$(tail -1 $file)"
-	if [[ $line =~ $partitionregex ]]; then
-		local p=${BASH_REMATCH[1]}
-		local start=${BASH_REMATCH[2]}
-		local size=${BASH_REMATCH[3]}
-		local id=${BASH_REMATCH[4]}
-
-		if (( $id != 83 )); then
-			echo "assertion 1 failed"
-			exit
-		fi
-
-		(( sumSize = ( start + size) * sectorSize ))
-		echo "$sumSize"
-	else
-		echo "assertion 2 failed"
-		exit
-	fi
-}
-
-for testfile in "${TEST_FILES[@]}"; do
-	file=$(cut -f 1 -d ' ' <<< "$testfile")
-	size=$(cut -f 2 -d ' ' <<< "$testfile")
-	test_calcSumSizeFromSFDISK $file $size
-done
